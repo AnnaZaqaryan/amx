6f21c41c08a1071db34ebc84d9abad257d64cc04
tiko dem <anahit.zakaryan1992@gmail.com>
2022-02-22 22:49:31 +0400

    fix mobile

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import CombineImg from '../assets/images/Combine.png';
 
@@ -7,26 +7,41 @@ import Torepresent from '../assets/images/Torepresent.png';
 import Rectangle from '../assets/images/rectangle.png';
 import Dots from '../assets/images/dots.png';
 import Bg from '../assets/images/BG.png';
+import WaveMobile from '../assets/images-mobile/wave-mobile.png';
+import DotsMobile from '../assets/images-mobile/dots-mobile.png';
+import RectangleMobile from '../assets/images-mobile/rectangle-mobile.png';
+import CombineMobile from '../assets/images-mobile/Combine-mobile.png';
+import CoolImageMobile from '../assets/images-mobile/Cool-images-mobile.png';
+import BgMobile from '../assets/images-mobile/BG-mobile.png';
 
 import './styles.css';
 
 const Main = () => {
 
     return (
-        <Container>
-            <Row className="main">
-                <Col xs={6} md={10} lg={12}>
-                    <div className="backgroundImage" style={{ backgroundImage: `url(${Rectangle})` }}></div>
-                    <img className='donsImg' src={Dots} />
-                    <img className='combineImg' src={CombineImg} />
-                    <img className='bg' src={Bg} />
-                </Col>
-                <Col xs={6} md={10} lg={12}>
+        <Fragment>
+            <div className='backgWrappMobile'>
+                <img className='backgroundMobile' src={WaveMobile} />
+                <img className='rectangleMobile' src={RectangleMobile} />
+                <img className='dotsMobile' src={DotsMobile} />
+            </div>
+            <Container>
+                <Row className="main">
+                    <Col xs={6} md={12} lg={12}>
+                        <div className="backgroundImage" style={{ backgroundImage: `url(${Rectangle})` }}></div>
+                        <img className='donsImg' src={Dots} />
+                        <img className='combineImg' src={CombineImg} />
+                        <img className='bg' src={Bg} />
+                        <img className='combineMobile' src={CombineMobile} />
+                        <img className='coolMobile' src={CoolImageMobile} />
+                        <img className='bgMobile' src={BgMobile} />
+                    </Col>
+                    {/* <Col xs={6} md={10} lg={12}>
                     <img className='torepresent' src={Torepresent} />
-
-                </Col>
-            </Row>
-        </Container>
+                </Col> */}
+                </Row>
+            </Container>
+        </Fragment>
     )
 }
 
